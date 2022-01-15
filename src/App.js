e8f54de8c985f588b70b2339e9b70a4028c2aed9
smartDevel <57102149+smartDevel@users.noreply.github.com>
2022-01-15 11:50:40 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           <link rel="icon" href={smile} />
         </Helmet>
         <AppBar>
-          <AppBar.Item path="javascript:alert('No point in that...');">Login</AppBar.Item>
-          <AppBar.Item path="https://bad-jokes-blog.netlify.app/">Blog</AppBar.Item>
+          <AppBar.Item path="javascript:alert('not available...');">Login</AppBar.Item>
+          {/* <AppBar.Item path="https://bad-jokes-blog.netlify.app/">Blog</AppBar.Item> */}
+          <AppBar.Item path="https://ways4.eu/">Blog</AppBar.Item>
           <AppBar.Logo>
             <Text size="lg">BadJokes </Text>
             <img style={{height: '39px'}} src={smile} alt="smile logo"></img>
